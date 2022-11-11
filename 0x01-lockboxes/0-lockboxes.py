93fbfa2e3bd49215bf6f250f97dd19688dc2ecaa
dahmie-code <dahmie@gmail.com>
2022-11-11 22:20:53 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    """
-    You have n number of locked boxes in front of you. Each box is numbered sequentially 
-    from 0 to n - 1 and each box may contain keys to the other boxes. 
-    """
-    firstBox = [0]
-    for key in firstBox:
-        for keyBox in boxes[key]:
-            if keyBox not in firstBox:
-                if keyBox < len(boxes):
-                    firstBox.append(keyBox)
-    if len(firstBox) == len(boxes):
+    """ method that determines if all the boxes can be opened. """
+    n = len(boxes)
+    myList = [0]
+    for i in myList:
+        for j in boxes[i]:
+            if j not in myList:
+                if j < n:
+                    myList.append(j)
+    if len(myList) == n:
         return True
     return False
