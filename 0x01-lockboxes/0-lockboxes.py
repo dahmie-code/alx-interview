687bc05e211cac220cfb2398f419018d7040000b
dahmie-code <dahmie@gmail.com>
2022-11-11 22:04:55 +0100

    lockboxes and pascal triangle completed

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    """
+    You have n number of locked boxes in front of you. Each box is numbered sequentially 
+    from 0 to n - 1 and each box may contain keys to the other boxes. 
+    """
+    boxOne = [0]
+    for key in boxOne:
+        for keyBox in boxes[key]:
+            if keyBox not in boxOne:
+                if keyBox < len(boxes):
+                    boxOne.append(keyBox)
+    if len(boxOne) == len(boxes):
+        return True
+    return False
